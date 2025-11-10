2a3e8d8bfbe1301261323078826c6d3bd213d1db
Erica Nunes <ericanunes2013ff@gmail.com>
2025-11-09 22:28:54 -0300

    Adicionando telas de Guia e Anhitrião (Painel)

--- app/src/main/java/com/example/faraway/MainActivity.kt
+++ app/src/main/java/com/example/faraway/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.faraway.ui.screen.GuidePanelScreen
+import com.example.faraway.ui.screen.PainelDoAnfitriaoScreen
 import com.example.faraway.ui.screen.MainScreen
 import com.example.faraway.ui.screen.TripsScreen
 import com.example.faraway.ui.theme.FarAwayTheme
@@ -34,7 +33,7 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Destinations.EXPLORE_ROUTE
+        startDestination = Destinations.GUIDE_DASHBOARD_ROUTE
     ) {
         composable(Destinations.EXPLORE_ROUTE) {
             MainScreen(navController = navController)
@@ -48,5 +47,17 @@ fun AppNavigation() {
         composable(Destinations.SOCIAL_ROUTE) { /* SocialScreen(navController) */ }
         composable(Destinations.CHAT_ROUTE) { /* ChatScreen(navController) */ }
         composable(Destinations.PROFILE_ROUTE) { /* ProfileScreen(navController) */ }
+
+
+        // Painel do Guia
+        composable(route = Destinations.GUIDE_DASHBOARD_ROUTE) {
+            GuidePanelScreen()
+        }
+
+        // Painel do Anfitrião
+        composable(route = Destinations.HOST_DASHBOARD_ROUTE) {
+            PainelDoAnfitriaoScreen() // <-- NOME CORRETO
+        }
     }
+
 }
--- app/src/main/java/com/example/faraway/ui/theme/Color.kt
+++ app/src/main/java/com/example/faraway/ui/theme/Color.kt
@@ -15,3 +15,14 @@ val PrimaryDark = Color(0xFF193568) // Azul Escuro do Header
 val AccentColor = Color(0xFF00BCD4) // Azul Claro do Botão "Ver Perfil" e Ícone "Explorar"
 val TagColor = Color(0xFF4DD0E1) // Azul da Tag "Guia"
 val LightGray = Color(0xFFF0F0F0) // Fundo dos botões não selecionados
+
+
+// Cores da tela do Painel do Guia
+val GuideHostTopBarBlue = Color(0xFF113162)
+val CurrentInfoCardBlue = Color(0x8C8BE3FA)
+val NotificationBadgeBlue = Color(0xFF00D4FF)
+val AcceptButtonColor = Color(0xFF00C8D7)
+val DeclineButtonColor = Color(0xFFF44336)
+val DeclineButtonColor2 = Color(0x80F44336)
+val PendingButtonColor = Color(0xFFFFEB3B)
+val  PendingButtonColor2 = Color(0xFFA65F00)
